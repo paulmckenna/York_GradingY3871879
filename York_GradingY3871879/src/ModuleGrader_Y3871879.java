ced942c6882bc25d08e29a879ea6655ff610a27b
paulmckenna <paulmckenna@pauls-mbp>
2019-05-10 18:14:47 +0100

    First code: outline of grade class

---
+++ York_GradingY3871879/src/ModuleGrader_Y3871879.java
@@ -0,0 +1,42 @@
+
+public class ModuleGrader_Y3871879 {
+
+	int mark;
+	
+
+	
+	public String gradeModule ()
+	
+	int mark;
+	
+	{
+	 if (mark < 40)
+	 {
+		 return  "Fail";
+	 }
+	 
+	 else if (mark > 39 && mark < 50 )
+		 
+	 {
+		 return  "Compensatable Fail";
+	}
+	
+	 else if (mark > 49 && mark < 60)
+		 
+	 {
+		 return "Satisfactory";
+	 }
+	 else if (mark > 59 && mark < 70)
+		 
+	 {
+		 return "Good";
+	 }
+	 
+else if (mark > 69 && mark < 101)
+		 
+	 {
+		 return "Excellent";
+	 } 
+		 
+}
+}
